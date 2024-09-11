600315642862ee126a3b63d1e930b26bb3475fb0
Tarandeep9988 <tarandeep65singh@gmail.com>
2024-09-11 19:37:47 +0530

    calculator version 1

--- calculator/script.js
+++ calculator/script.js
@@ -1,18 +1,46 @@
 const numBtn = document.querySelectorAll('.num-btn');
+const pointBtn = document.getElementById('point-btn');
+const opBtn = document.querySelectorAll('.op-btn');
 const displayBox = document.querySelector('#disp_field');
-let ans = 0;
+let ans = '0';
 
-for (const each_btn of numBtn) {
-    each_btn.addEventListener('click', (e) => {
-        const btnVal = Number(each_btn.value);
-        if (ans === 0) {
-            ans = btnVal;
-            console.log(ans);
+function displayResult(btnValue) {
+    if (true) {
+        if (ans === '0') {
+            ans = btnValue;
         }
         else {
-            ans = (ans * 10) + btnVal;
+            ans += btnValue;
         }
-        displayBox.innerHTML = String(ans);
-        console.log(ans);
+        displayBox.innerHTML = ans;
+    }
+}
+
+// Event listeners for number buttons
+for (const each_btn of numBtn) {
+    each_btn.addEventListener('click', (e) => {
+        displayResult(each_btn.value)
     });
 }
+// Event listeners for point button
+pointBtn.addEventListener('click', function(){
+    if (!(ans.includes(pointBtn.value))) {
+        ans += pointBtn.value;
+        displayBox.innerHTML = ans;
+    }
+});
+
+// Event listeners for operation buttons
+for (const each_btn of opBtn) {
+    each_btn.addEventListener('click', ()=>{
+        if (each_btn.value === '=') {
+            displayBox.innerHTML = eval(ans);
+        }
+        else {
+            ans += each_btn.value;
+
+            displayBox.innerHTML = ans;
+        }
+    })
+}
+
