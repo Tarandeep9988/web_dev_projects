4c0c044753e2ac1ea9877d666fc6a3ee777f7d02
Tarandeep9988 <tarandeep65singh@gmail.com>
2024-09-27 11:52:35 +0530

    number base converter successfully build

--- base_converter/script.js
+++ base_converter/script.js
@@ -4,6 +4,8 @@ const toSelection = document.getElementById('to-base');
 const convertBtn = document.getElementById('convert-btn');
 const resultField = document.getElementById('result-box');
 const resetBtn = document.getElementById('reset-btn');
+const swapBtn = document.querySelector('#swap-btn');
+const copyBtn = document.querySelector('#copy-btn');
 
 convertBtn.addEventListener('click', function() {
     const fromBase  = fromSelection.value;
@@ -24,4 +26,29 @@ convertBtn.addEventListener('click', function() {
 resetBtn.addEventListener('click', ()=> {
     inputField.value = "";
     resultField.value = "";
-})
+});
+
+swapBtn.addEventListener('click', () => {
+    const fromVal = fromSelection.value;
+    const toVal = toSelection.value;
+    for (const element of fromSelection) {
+        if (element.value == toVal) {
+            element.setAttribute('selected', true);
+        }
+        else {
+            element.removeAttribute('selected');
+        }
+    }
+    for (const element of toSelection) {
+        if (element.value == fromVal) {
+            element.setAttribute('selected', true);
+        }
+        else {
+            element.removeAttribute('selected');
+        }
+    }
+});
+
+copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(resultField.value);
+});
